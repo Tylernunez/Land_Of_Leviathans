b11c0ae6b8997d6f21e3cba22119e1846e36a770
James Ostarly <james.ostarly@selu.edu>
2017-07-23 19:09:04 -0500

    almost workable for text box interactions -> put panel into OpenPanel variable

---
+++ Land of Leviathans/Assets/Interact.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour {
+public GameObject OpenPanel = null;
+
+	// Use this for initialization
+	void Start () {
+        OpenPanel.SetActive(false);
+	}
+	
+    void OnTriggerEnter(GameObject other)
+    {
+        if (other.tag == "Player")
+        {
+            OpenPanel.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(GameObject other)
+    {
+        if (other.tag == "Player")
+        {
+            OpenPanel.SetActive(false);
+        }
+
+    }
+
+    private bool IsOpenPanelActive
+    {
+        get
+        {
+            return OpenPanel.activeInHierarchy;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (IsOpenPanelActive)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                OpenPanel.SetActive(false);
+            }
+        }
+        if (IsOpenPanelActive == false)
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                OpenPanel.SetActive(true);
+            }
+        }
+    }
+}
